d217e75c9b1948811ab0aca0cb9ddd6ef4569d73
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2023-12-20 18:27:45 +0530

    new

---
+++ src/components/Signin.js
@@ -0,0 +1,19 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+const Signin = () => {
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={6}>
+          authetication
+        </Grid>
+        <Grid item xs={6}>
+          developer
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Signin;
